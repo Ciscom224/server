89774358a151131df74aa36b83141d77ae0e8ff4
ciscom224 <mamadouciss224e@gmail.com>
2024-05-10 19:51:23 +0200

    mise a jour admin

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ io.on('connection',(socket) => {
     
 })
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser())
 
 // jwt 
