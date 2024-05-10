5caf0176e59cb8a32af533e650fbc4584eb09d81
ciscom224 <mamadouciss224e@gmail.com>
2024-05-10 21:36:41 +0200

    mise a jour image

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -5,7 +5,6 @@ const path = require("path");
 const { uploadErrors } = require("../utils/errors.utils");
 
 module.exports.uploadProfil = async (req, res) => {
-    console.log(__dirname)
     const { id } = req.params;
     const { image } = req.body;
     if (!objID.isValid(id))
@@ -22,23 +21,12 @@ module.exports.uploadProfil = async (req, res) => {
         if (!user) {
             return res.status(400).json({ message: "Utilisateur non trouvé." });
         }
-
-        // le nom et le chemin d'acces du dossier du client 
-        const fileName = `${user.surName}.jpeg`;
-        const imagePath = path.join(__dirname, `../../client/public/images/Profils/${fileName}`);
-
-        // ajout dans chez le client 
-        fs.writeFileSync(imagePath, image, 'base64');
-
         // Mise à jour du chemin de l'image dans le modèle d'utilisateur
         user.profilImage = image ;
-
         const updatedUser = await user.save();
-
         res.status(200).json({ user: updatedUser });
     } catch (error) {
-        console.error("Erreur lors de l'upload de l'image :", error);
         const errors = uploadErrors(error);
-        res.status(400).json({ errors });
+        res.status(400).json(error);
     }
 };
