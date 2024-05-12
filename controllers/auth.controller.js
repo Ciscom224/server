6981b9cd5510f84634c389116185b4daa456fae6
D-Farouk <faroukdell@hotmail.com>
2024-05-12 02:26:41 +0200

    Changement de la récupération du body dans login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const UserModel=require('../models/user.model');
 const jwt=require('jsonwebtoken');
 const { signUpErrors, signInErrors } = require('../utils/errors.utils');
+const bcrypt = require('bcrypt');
 
 const maxDate =24 * 60 * 60 * 1000
 
@@ -27,19 +28,31 @@ module.exports.signIn=async (req,res)=>{
     const {email,password}=req.body;
 
     try {
-        const user= await UserModel.login(email.trim(),password.trim());
-        const token=createToken(user._id);
-        res.cookie('jwt',token,{httpOnly:true,sameSite: 'None',secure:true,maxAge:maxDate}) // ajout du token JWT dans le cookie
-        res.status(201).send(user)
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            throw new Error("email error");
+        }
+
+        const auth = await bcrypt.compare(password, user.password);
+        if (!auth) {
+            throw new Error("password error");
+        }
+
+        // Mettre à jour le champ 'online' à true
+        await UserModel.updateOne({ email }, { $set: { online: true } });
+
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, sameSite: 'None', secure: true, maxAge: maxDate });
+        res.status(201).send(user);
     } catch (err) {
-        const errors=signInErrors(err)
-        res.status(200).send(errors)
+        const errors = signInErrors(err);
+        res.status(200).send(errors);
     }
 
 }
 module.exports.logout= async (req,res)=>{
 
    await UserModel.updateOne({ _id: res.locals.user._id }, { $set: { online: false } });
-   res.clearCookie('jwt') // ajout du token JWT dans le cookie
+   res.cookie('jwt',"",{httpOnly:true,sameSite: 'None',secure:true,maxAge:1}) // ajout du token JWT dans le cookie
    res.status(201).send({message:"vous vous etes deconnecter"})
 }
